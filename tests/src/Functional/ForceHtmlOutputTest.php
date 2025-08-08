fee5ab60fe3492350cda0f6df0c03a5ecdc9af0c
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2025-08-08 11:15:11 +0200

    fix: use relative path for BROWSERTEST_OUTPUT_DIRECTORY and add HTML output test
    - Change BROWSERTEST_OUTPUT_DIRECTORY from absolute /tmp/simpletest_output to relative sites/simpletest/browser_output
    - This matches Drupal's default configuration and should allow HtmlOutputLogger to initialize properly
    - Add --verbose flag to PHPUnit to see extension initialization messages
    - Add ForceHtmlOutputTest that explicitly generates HTML output and fails to test artifact upload
    - Simplify debugging output to focus on the single output directory
    
    The relative path should work better with PHPUnit's working directory expectations.

---
+++ tests/src/Functional/ForceHtmlOutputTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\proxy_block\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test to force HTML output generation for debugging CI issues.
+ *
+ * @group proxy_block
+ */
+class ForceHtmlOutputTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['proxy_block'];
+
+  /**
+   * Force HTML output generation by visiting a page and failing.
+   */
+  public function testForceHtmlOutput(): void {
+    // Visit a page to generate browser state
+    $this->drupalGet('<front>');
+    
+    // Check page loaded
+    $this->assertSession()->statusCodeEquals(200);
+    
+    // Force HTML output by explicitly calling htmlOutput
+    if (method_exists($this, 'htmlOutput')) {
+      $this->htmlOutput();
+    }
+    
+    // Now intentionally fail to trigger artifact upload
+    $this->fail('Intentionally failing to test HTML artifact generation in CI');
+  }
+
+}
